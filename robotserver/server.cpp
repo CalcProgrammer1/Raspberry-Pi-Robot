a007902d42c9b84893e1bdf584c44a4b152fbc58
Adam Honse <calcprogrammer1@gmail.com>
2015-12-16 01:11:38 +0000

    Handle disconnects by stopping servos to prevent robot from running away.

--- robotserver/server.cpp
+++ robotserver/server.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 
     int numbytes = recv(clientfd,&pad,sizeof pad,0);
 
-    while(numbytes == sizeof(pad))
+    while( numbytes == sizeof( pad ) )
     {
         char  servo_str[128];
         float axis_0;
@@ -114,8 +114,6 @@ int main(int argc, char *argv[])
             camera_y = 50.0f;
         }
 
-       // cout << axis_0 << "		" << axis_1 << "		" << axis_2 << "		" << axis_3 << endl;
-
         sprintf( servo_str, "echo 0=%d\% > /dev/servoblaster", (int)( 100 - axis_1 ) );
         system( servo_str );
 
@@ -131,122 +129,12 @@ int main(int argc, char *argv[])
         numbytes = recv(clientfd,&pad,sizeof pad,0);
     }
 
+    printf("numbytes = %d\r\n", numbytes);
+
+    system( "echo 0=50\% > /dev/servoblaster" );
+    system( "echo 1=50\% > /dev/servoblaster" );
+    system( "echo 3=50\% > /dev/servoblaster" );
+    system( "echo 4=50\% > /dev/servoblaster" );
+
     close(clientfd);
 }
-
-
-
-#if 0
-  addrinfo hints, *result;
-  int fd;
-
-  sockaddr_storage serv_addr;
-  int yes = 1;
-  
-  memset(&hints, 0, sizeof hints);
-
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-
-  getaddrinfo(argv[1], PORT, &hints, &result);
-
-  fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-  
-  connect(fd, result->ai_addr, result->ai_addrlen);
-
-  int numbytes = 0;
-
-  numbytes = recv(fd,&pad,sizeof pad,0);
-
-  while(numbytes > 0)
-  {
-    if(numbytes > 0)
-    {
-      char  servo_str[128];
-      float axis_0;
-      float axis_1;
-      float axis_2;
-      float axis_3;
-
-      int   axis_4;
-      int   axis_5;
-
-      axis_0 = pad.aPos[0];
-      axis_1 = pad.aPos[1];
-      axis_2 = pad.aPos[2];
-      axis_3 = pad.aPos[3];
-
-      axis_0 = axis_0 / 32767.0f;
-      axis_1 = axis_1 / 32767.0f;
-      axis_2 = axis_2 / 32767.0f;
-      axis_3 = axis_3 / 32767.0f;
-
-      axis_0 = ( axis_0 * 50.0f ) + 50.0f;
-      axis_1 = ( axis_1 * 20.0f ) + 50.0f;
-      axis_2 = ( axis_2 * 50.0f ) + 50.0f;
-      axis_3 = ( axis_3 * 20.0f ) + 50.0f;
-
-      if( pad.aPos[4] < 0 )
-      {
-        camera_x -= 0.5f;
-        if( camera_x < 0.0f )
-        {
-          camera_x = 0.0f;
-        }
-      }
-      else if( pad.aPos[4] > 0 )
-      {
-        camera_x += 0.5f;
-        if( camera_x > 100.0f )
-        {
-          camera_x = 100.0f;
-        }
-      }
-
-      if( pad.aPos[5] < 0 )
-      {
-        camera_y -= 0.5f;
-        if( camera_y < 0.0f )
-        {
-          camera_y = 0.0f;
-        }
-      }
-      else if( pad.aPos[5] > 0 )
-      {
-        camera_y += 0.5f;
-        if( camera_y > 100.0f )
-        {
-          camera_y = 100.0f;
-        }
-      }
-
-      if( pad.bPos[0] != 0 )
-      {
-        camera_x = 50.0f;
-        camera_y = 50.0f;
-      }
-
-      //cout << axis_0 << "		" << axis_1 << "		" << axis_2 << "		" << axis_3 << endl;
-
-      sprintf( servo_str, "echo 0=%d\% > /dev/servoblaster", (int)( 100 - axis_1 ) );
-      system( servo_str );
-
-      sprintf( servo_str, "echo 1=%d\% > /dev/servoblaster", (int)axis_3 );
-      system( servo_str );
-
-      sprintf( servo_str, "echo 2=%f\% > /dev/servoblaster", 100.0f - camera_x );
-      system( servo_str );
-
-      sprintf( servo_str, "echo 3=%f\% > /dev/servoblaster", 100.0f - camera_y );
-      system( servo_str );
-
-
-      usleep(25000);
-    }
-    numbytes = recv(fd,&pad,sizeof pad,0);
-  }
-  close(fd);
-  return 0;
-}
-
-  #endif
